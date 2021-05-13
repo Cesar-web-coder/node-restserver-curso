7d71a397004f900d9ec9432a67636b2e62e90283
Cesar Alfonso Acevedo Sepulveda <cesitar-lion@hotmail.com>
2021-05-13 17:19:56 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -13,6 +13,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://trakex:akashh123@cluster0.4o1g5.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
